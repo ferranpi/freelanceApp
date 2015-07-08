0d9644512dd2f56c3a09b1ba00ebc0763a8c4b55
Ferran Pi <ferran.pi@gmail.com>
2015-07-08 18:37:27 +0200

    Angular basic test

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,37 +1,42 @@
-(function () {
-  'use strict';
-    /**
-     * Main module of the application.
-     */
-  angular
-    .module('freelanceApp', [
-      'ngAnimate',
-      'ngAria',
-      'templates',
-      'ngRoute',
-      'ngResource',
-      'ngMaterial'
-    ])
-    //.config(function ($routeProvider, $mdThemingProvider, $mdIconProvider) {
-    .config(function($routeProvider){
 
-      $routeProvider
-        .when('/', {
-          templateUrl: 'index.html'
-          //controller: 'MainController'
-        })
-        .otherwise({
-          redirectTo: '/'
-        });
-    //   //$mdIconProvider
-    //   //  .defaultIconSet("./assets/svg/avatars.svg", 128)
-    //   //  .icon("share", "./assets/svg/share.svg", 24)
-    //   //  .icon("google_plus", "./assets/svg/google_plus.svg", 512)
-    //   //  .icon("hangouts", "./assets/svg/hangouts.svg", 512)
-    //   //  .icon("twitter", "./assets/svg/twitter.svg", 512)
-    //   //  .icon("phone", "./assets/svg/phone.svg", 512);
-    //   $mdThemingProvider.theme('default')
-    //     .primaryPalette('blue')
-    //     .accentPalette('pink');
-    });
-})();
+'use strinc';
+angular.module('freelanceApp',[]);
+// (function () {
+//   'use strict';
+//     /**
+//      * Main module of the application.
+//      */
+//   angular
+//     .module('freelanceApp', [
+//       'ngAnimate',
+//       'ngAria',
+//       'templates',
+//       'ngRoute',
+//       'ngResource',
+//       'ngMaterial'
+//     ])
+//     .config(function ($routeProvider, $mdThemingProvider, $mdIconProvider) {
+//       $routeProvider
+//         .when('/', {
+//           templateUrl: 'index.html'
+//           //controller: 'MainController'
+//         })
+//         .when('/contactos', {
+//           templateUrl: 'scripts/contactos/views/list.html'
+//           //controller: 'MainController'
+//         })
+//         .otherwise({
+//           redirectTo: '/'
+//         });
+//     //   //$mdIconProvider
+//     //   //  .defaultIconSet("./assets/svg/avatars.svg", 128)
+//     //   //  .icon("share", "./assets/svg/share.svg", 24)
+//     //   //  .icon("google_plus", "./assets/svg/google_plus.svg", 512)
+//     //   //  .icon("hangouts", "./assets/svg/hangouts.svg", 512)
+//     //   //  .icon("twitter", "./assets/svg/twitter.svg", 512)
+//     //   //  .icon("phone", "./assets/svg/phone.svg", 512);
+//     //   $mdThemingProvider.theme('default')
+//     //     .primaryPalette('blue')
+//     //     .accentPalette('pink');
+//     });
+// })();
